77664d3039bd713aaa48ba9123d208b0e3ef69b1
Jordan Wharmby <Jordanmod@hotmail.com>
2020-03-31 15:05:56 +0100

    Clean Solution Receaveer
    No errors

--- StockTrading.Receiver/Services/StockService.cs
+++ StockTrading.Receiver/Services/StockService.cs
@@ -1,4 +1,4 @@
-﻿using StockTrading.Receiver.Methods;
+﻿using StockTrading.Sender.Mappers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace StockTrading.Receiver.Services {
     public class StockService : IStockService {
-        private readonly StockRepository _stockRepository;
-        private readonly IMapper _map;
+        private readonly IStockRepository _stockRepository;
+        public readonly IMapper _mapper;
+
+        public StockService(IStockRepository stockRepository, IMapper mapper) {
+            _stockRepository = stockRepository;
+            _mapper = mapper;
+        }
+
     }
 }
--- StockTrading.Receiver/Startup.cs
+++ StockTrading.Receiver/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -12,30 +13,16 @@ using Microsoft.Extensions.Logging;
 
 namespace StockTrading.Receiver {
     public class Startup {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddControllers();
+            services.AddMvc();
+            //services.AddAWSService<IAmazonDynamoDB>();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints => {
-                endpoints.MapControllers();
-            });
+            app.UseMvc();
         }
     }
 }
