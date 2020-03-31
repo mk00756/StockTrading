0b1ebdc7da813d5cff74fc0d856193e952af48d7
Jordan Wharmby <Jordanmod@hotmail.com>
2020-03-31 11:56:43 +0100

    Receiver: Basic Infuilstructure
    Basic infrastructure for receiving

---
+++ StockTrading.Receiver/Methods/Mapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrading.Receiver.Methods {
+    public class Mapper : IMapper {
+    }
+}
---
+++ StockTrading.Receiver/Models/StockDB.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace StockTrading.Receiver.Models {
+    [DynamoDBTable("StockTracking")]
+    public class StockDB {
+        [DynamoDBHashKey]
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
