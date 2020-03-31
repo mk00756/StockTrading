f7d2809f7e22263e55d5b16db60e55ee65f7f632
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-03-31 14:52:01 +0100

    ReceiveMQ Added

---
+++ StockTrading.Receiver/MessageBroker/ReceiveMQ.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrading.Receiver.MessageBroker
+{
+    public class ReceiveMQ
+    {
+        public void ReceiveStock()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "stocks", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "stocks",
+                              routingKey: "");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                // TODO: Further process the message
+                
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+    }
+}
