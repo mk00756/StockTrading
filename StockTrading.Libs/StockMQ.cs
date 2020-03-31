0e942666cd3e35de1ae906f01443e05d0e9d2019
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-03-31 11:52:29 +0100

    StockMQ can send and receive messages

---
+++ StockTrading.Libs/StockMQ.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace StockTrading.Libs
+{
+    public class StockMQ
+    { 
+        public void SendStock(string msg)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "stocks", type: ExchangeType.Fanout);
+
+            var message = msg;
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "stocks",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        public void ReceiveStock()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "stocks", type: ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "stocks",
+                              routingKey: "");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                // TODO: Further process the message
+
+                };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+    }
+}
