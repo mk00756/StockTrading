8bf72ddbb44233cec7fc1a0afcd555c7b6164ca2
Jordan Wharmby <Jordanmod@hotmail.com>
2020-03-31 10:53:33 +0100

    Added a model

---
+++ StockTrading/Models/StockModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTrading.Sender.Models {
+    public class StockModel {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
