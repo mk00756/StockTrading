ee4c7612635836644703723a136db9c895b203f9
nathanyip98 <nathanyi98@yahoo.co.uk>
2020-03-31 11:10:46 +0100

    Added model

---
+++ StockTrading/Models/StockDB.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace StockTrading.Sender.Models
+{
+    [DynamoDBTable("StockTracking")]
+
+    public class StockDB
+    {
+        [DynamoDBHashKey]
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
