e82192cda388b071d18fcc5e152a332c34bacba3
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-03-31 15:38:21 +0100

    Fixed view not displayinh

--- StockTrading.Receiver/Startup.cs
+++ StockTrading.Receiver/Startup.cs
@@ -21,7 +21,7 @@ namespace StockTrading.Receiver {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddControllers();
         }
 
