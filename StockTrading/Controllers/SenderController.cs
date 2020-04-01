cf120e49ec3d8885656a5f2df838c06ae542bfdf
tjwhittam <tjwhittam@btinternet.com>
2020-04-01 10:50:20 +0100

    trial push and pull

--- StockTrading/Controllers/SenderController.cs
+++ StockTrading/Controllers/SenderController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StockTrading.Sender.Services;
 
 namespace StockTrading.Sender.Controllers
 {
+   //[Route ("sender")]
     public class SenderController : Controller
     {
-	//a comment
+        private readonly ISenderService _SenderService;
+
+        public SenderController (ISenderService senderSevice)
+        {
+            _SenderService = senderSevice;
+        }
     }
+
+    [HttpPost]
+    //[Route ("addstock")]
+    public async Task<IActionResult> AddStock(string Name, double price)
 }
