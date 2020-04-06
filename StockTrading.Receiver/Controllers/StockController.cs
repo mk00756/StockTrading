3fb0e3d90f1a1b5cb2048171613c0802b83d73ca
Jordan Wharmby <Jordanmod@hotmail.com>
2020-04-06 14:57:06 +0100

    Graph

--- StockTrading.Receiver/Controllers/StockController.cs
+++ StockTrading.Receiver/Controllers/StockController.cs
@@ -9,8 +9,6 @@ using Newtonsoft.Json;
 using System.Text;
 using RabbitMQ.Client.Events;
 using StockTrading.Receiver.Contracts;
-using RabbitMQ.Client.MessagePatterns;
-using StockTrading.Receiver.MessageBroker;
 
 namespace StockTrading.Receiver.Controllers {
 
@@ -19,60 +17,49 @@ namespace StockTrading.Receiver.Controllers {
     public class StockController : ControllerBase {
 
         public readonly IStockService _stockServer;
-        private const string ExchangeName = "Topic_Exchange";
-        private const string AllQueueName = "AllTopic_Queue";
-        private string returnMessage = "";
 
         public StockController(IStockService stockService) {
             _stockServer = stockService;
-            //ReceiveMessage();
+            ReceiveMessage();
         }
         private void ReceiveMessage() {
             //Vreates the conmection
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.ExchangeDeclare(ExchangeName, "topic");
-            channel.QueueDeclare(AllQueueName, true, false, false, null);
-
-            channel.QueueBind(AllQueueName, ExchangeName, "stocks.add");
-            channel.BasicQos(0, 10, false);
-
-
+            channel.ExchangeDeclare(exchange: "stocks", type: ExchangeType.Fanout);
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,exchange: "stocks",routingKey: "");
             var consumer = new EventingBasicConsumer(channel);
-
-            consumer.Received += (model, ea) => {
+            consumer.Received += async (model, ea) => {
                 var body = ea.Body;
                 var message = Encoding.UTF8.GetString(body);
                 var stock = JsonConvert.DeserializeObject<StockDB>(message);
-                returnMessage = stock.ToString();
-
-                // TODO: Further process the message
+                var newStock = new StockRespons {
+                    Name = stock.Name,
+                    Price = Convert.ToInt32(stock.Price)
+                };
+                await PostFromRabbitMQ(newStock);
 
             };
+            channel.BasicConsume(queue:queueName,autoAck: true,consumer:consumer);
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllItemsFromDatabase() {
-            //var result = await _stockServer.GetAllItemsFromDatabase();
-            ReceiveMQ receive = new ReceiveMQ(_stockServer);
-            receive.CreateConnection();
-            await receive.ReceiveMessage();
-            receive.Close();
+        public async Task<IEnumerable<StockRespons>> GetAllItemsFromDatabase() {
+            var result = await _stockServer.GetAllItemsFromDatabase();
+            return result;
+        }
 
-
+        //Posts from RabbitMQ
+        private async Task<IActionResult> PostFromRabbitMQ(StockRespons stock) {
+            await _stockServer.AddStock(stock);
             return Ok();
         }
 
-        ////Posts from RabbitMQ
-        //private async Task<IActionResult> PostFromRabbitMQ(StockRequest stock) {
-        //    await _stockServer.AddStock(stock);
-        //    return Ok();
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> AddNewStocks([FromBody] StockRequest stockRequest) {
-        //    await _stockServer.AddStock(stockRequest);
-        //    return Ok();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> AddNewStocks([FromBody] StockRespons stockRequest) {
+            await _stockServer.AddStock(stockRequest);
+            return Ok();
+        }
     }
 }
