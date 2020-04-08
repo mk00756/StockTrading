f5cf787f7d9f5c3afe24a531d61e20b7407a9c47
nathanyip98 <nathanyip98@yahoo.co.uk>
2020-04-08 16:00:41 +0100

    Add works

--- react/ClientApp/src/components/AddStock.js
+++ react/ClientApp/src/components/AddStock.js
@@ -1,78 +1,73 @@
-﻿//import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 
-//export class AddStock extends Component {
-//    static displayName = AddStock.name;
+export class AddStock extends Component {
+    static displayName = AddStock.name;
 
-//    constructor(props) {
-//        super(props);
-//        this.state = { stockName: '', stockPrice: '' };
-//    }
+    constructor(props) {
+        super(props);
+        this.state = { stockName: '', stockPrice: '' };
+    }
 
-//    handleChange = ({ target }) => {
-//        this.setState({ [target.name]: target.value });
-//    };
-
-//    onSubmit = event => {
-//        const name = this.stockName.value;
-//        const price = this.stockPrice.value;
-//        const info = { name: name, price: price };
-//        const data = this.state.data;
-
-//    }
+    handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    };
 
 
-//    addNewStock = () => {
-//        const name = this.stockName.value;
-//        const price = this.stockPrice.value;
 
-//        const requestOptions = {
-//            method: 'POST',
-//            headers: { 'Content-Type': 'application/json' },
-//            body: JSON.stringify({
-//                "name": '"' +(name) +'",'
-//                "price": {this.stockPrice.value }
-//            })
+    // POST method is executed on button click
+    addNewStock = () => {
+        var name = this.state.stockName;
+        var price = parseInt(this.state.stockPrice);
 
-//        };
-//        fetch('stock', requestOptions)
-//            .then(response => response.json())
-//            .then(data => this.setState({ postId: data.id }));
-//    }
+        var value = { "name": name, "price": price };
+
+        // converts object to a JSON
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(value)
+
+        };
+        // POST Method
+        fetch('stock', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ postId: data.id }));
+    }
     
 
 
-//    render() {
-//        return (
-//            <React.Fragment>
+    render() {
 
-//                <form className="form-inline" onSubmit={this.onSubmit}>
-//                <label htmlFor="stockName">Stock Name</label>
-//                <input
-//                    type="text"
-//                    name="stockName"
-//                    placeholder="FTSE"
-//                    value={this.state.stockName}
-//                    onChange={this.handleChange}
-//                />
+        return (
+           <React.Fragment >
 
-//                <label htmlFor="stockPrice">Stock Price</label>
-//                <input
-//                    type="text"
-//                    placeholder="4000"
-//                    name="stockPrice"
-//                    value={this.state.stockPrice}
-//                    onChange={this.handleChange}
-//                    />
-//                    <button
-//                        type="submit"
-//                        className="btn btn-primary"
-//                        onClick={this.addNewStock}>Add
-//                        </button>
-//                </form>
+            <h1> Stock name and price is : {this.state.stockName} {this.state.stockPrice} </h1>
 
-//            <h1> Stock name and price is : {this.state.stockName} {this.state.stockPrice} </h1>
-//            </React.Fragment>
+            <form className="form-inline" onSubmit={this.onSubmit}>
+                <label htmlFor="stockName">Stock Name </label>
+                <input
+                    type="text"
+                    name="stockName"
+                    placeholder="FTSE"
+                    value={this.state.stockName}
+                    onChange={this.handleChange}
+                />
 
-//        );
-//    }
-//}
+                <label htmlFor="stockPrice">Stock Price </label>
+                <input
+                    type="number"
+                    placeholder="4000"
+                    name="stockPrice"
+                    value={this.state.stockPrice}
+                    onChange={this.handleChange}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    onClick={this.addNewStock}>Add
+                        </button>
+            </form>
+            </React.Fragment>
+        );
+    }
+}
