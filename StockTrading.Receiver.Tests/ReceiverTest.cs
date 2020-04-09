12607fc7a38fa42090e6b1a47c1c724ea2e61e97
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-04-09 15:05:48 +0100

    Added mapper test for ToStockContract

--- StockTrading.Receiver.Tests/ReceiverTest.cs
+++ StockTrading.Receiver.Tests/ReceiverTest.cs
@@ -61,11 +61,65 @@ namespace StockTrading.Receiver.Tests
 
             // Act
             result = mapper.ToDocument(stockResponse);
-
+            
             // Assert
             Assert.AreEqual(expectedDocument["Name"], result["Name"]);
             Assert.AreEqual(expectedDocument["Price"], result["Price"]);
             Assert.AreEqual(expectedDocument["LastUpdated"], result["LastUpdated"]);
+        }
+
+        [Test]
+        public void Mapper_ToStockContract_ReturnsStocks()
+        {
+            // Arrange
+            string currentTime = System.DateTime.Now.ToString();
+
+            var item1 = new Document();
+            item1["Name"] = "STOCK1";
+            item1["Price"] = 123.45f;
+            item1["LastUpdated"] = currentTime;
+
+            var item2 = new Document();
+            item2["Name"] = "STOCK2";
+            item2["Price"] = 111.11f;
+            item2["LastUpdated"] = currentTime;
+
+            List<Document> itemList = new List<Document>();
+            itemList.Add(item1);
+            itemList.Add(item2);
+
+            StockRespons expectedItem1 = new StockRespons();
+            expectedItem1.Name = "STOCK1";
+            expectedItem1.Price = 123.45f;
+            expectedItem1.LastUpdated = currentTime;
+
+            StockRespons expectedItem2 = new StockRespons();
+            expectedItem2.Name = "STOCK2";
+            expectedItem2.Price = 111.11f;
+            expectedItem2.LastUpdated = currentTime;
+
+            List<StockRespons> expectedItemList = new List<StockRespons>();
+            expectedItemList.Add(expectedItem1);
+            expectedItemList.Add(expectedItem2);
+
+            var mapper = new Mapper();
+
+            IEnumerable<StockRespons> result;
+
+            // Act
+            result = mapper.ToStockContract(itemList);
+
+            // Assert
+            var enumerator = result.GetEnumerator();
+            int count = -1;
+            while(enumerator.MoveNext())
+            {
+                count++;
+                Assert.AreEqual(expectedItemList[count].Name, enumerator.Current.Name);
+                Assert.AreEqual(expectedItemList[count].Price, enumerator.Current.Price);
+                Assert.AreEqual(expectedItemList[count].LastUpdated, enumerator.Current.LastUpdated);
+            }
+            
         }
     }
 }
