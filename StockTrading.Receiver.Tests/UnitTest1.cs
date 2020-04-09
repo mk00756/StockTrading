e4730f90875a8ed0573728af90b695b724427b41
Mateusz Kulinski <mkulinski1998@gmail.com>
2020-04-09 11:15:17 +0100

    Added Receiver Tests

---
+++ StockTrading.Receiver.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StockTrading.Receiver.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
