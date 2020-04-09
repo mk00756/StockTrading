830ce70e7883dd055375d8b39cf251153d8e59f7
Jordan Wharmby <Jordanmod@hotmail.com>
2020-04-09 15:23:39 +0100

    Tests

---
+++ WebTestingLibrary/StockTradeSenderWebTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Xunit;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebTestingLibrary {
+    public class StockTradeSenderWebTest {
+        [Fact]
+        public void OpenSenderHomePage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/");
+                //Checks if it is right
+                Assert.Equal("https://localhost:44372/", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void OpenSenderCreatePage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/add");
+                //Checks if it is right
+                Assert.Equal("https://localhost:44372/add", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void OpenSenderEdditPage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/update");
+                //Checks if it is right
+                Assert.Equal("https://localhost:44372/update", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void OpenSenderDeletePage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/delete");
+                //Checks if it is right
+                Assert.Equal("https://localhost:44372/delete", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void AddAStock() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/add");
+                //Veriables
+                string name = "TestStock";
+                double price = 350;
+                //Adds data to the text box#
+                driver.FindElement(By.Name("stockName")).SendKeys(name);
+                driver.FindElement(By.Name("stockPrice")).SendKeys(price.ToString());
+                driver.FindElement(By.XPath("/html/body/div/div/div/form/div[3]/button")).Click();
+
+            }
+        }
+
+        [Fact]
+        public void CheckStockEgsist() {
+            using(IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("https://localhost:44372/");
+                Assert.Equal("https://localhost:44372/", driver.Url);
+            }
+        }
+
+    }
+}
---
+++ WebTestingLibrary/StockTraderReceiverWebTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebTestingLibrary {
+    public class StockTraderReceiverWebTest {
+
+        [Fact]
+        public void OpenReceaverPage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("http://localhost:55236/Home");
+                //Checks if it is right
+                Assert.Equal("Stock Trader Receiver - Home", driver.Title);
+                Assert.Equal("http://localhost:55236/Home", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void ReloadReceaverHomePage() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("http://localhost:55236/Home");
+                //refreshes the page
+                driver.Navigate().Refresh();
+                //Checks if it is right
+                Assert.Equal("Stock Trader Receiver - Home", driver.Title);
+                Assert.Equal("http://localhost:55236/Home", driver.Url);
+            }
+        }
+
+        [Fact]
+        public void ClickSerchButton() {
+            using (IWebDriver driver = new ChromeDriver()) {
+                //Loads the page
+                driver.Navigate().GoToUrl("http://localhost:55236/Home");
+                //gets the serch button
+                IWebElement SerchButton = driver.FindElement(By.Id("serchButton"));
+                IWebElement InputBox = driver.FindElement(By.Id("stockName"));
+                IWebElement Output = driver.FindElement(By.Id("SubTitle"));
+                string input = "FTSE 100";
+                //Enter text
+                InputBox.SendKeys(input);
+                //Clicks the button
+                SerchButton.Click();
+                //Checks iof it has worked
+                Assert.Equal("Showing results for " + input, Output.Text);
+
+            }
+        }
+    }
+}
