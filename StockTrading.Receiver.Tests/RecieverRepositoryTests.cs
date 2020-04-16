c694493eafeeb89bc6bbf1c950bdee03e0e10486
tjwhittam <tjwhittam@btinternet.com>
2020-04-16 11:08:22 +0100

    Sender repository tests written; issues with rabbitMQ. test file structure altered for easier nav.

--- StockTrading.Receiver.Tests/RecieverRepositoryTests.cs
+++ StockTrading.Receiver.Tests/RecieverRepositoryTests.cs
@@ -1,4 +1,4 @@
-using Amazon.DynamoDBv2;
+﻿using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using NUnit.Framework;
 using StockTrading.Receiver.Contracts;
@@ -9,130 +9,10 @@ using System.Threading.Tasks;
 
 namespace StockTrading.Receiver.Tests
 {
-    public class Tests
+    public class ReceiverRepositoyTests
     {
         [SetUp]
-        public void Setup(){ }
-
-        [Test]
-        public void Mapper_ToStockContract_ReturnsStockResponse()
-        {
-            // Arrange
-            string currentTime = System.DateTime.Now.ToString();
-            var item = new Document
-            {
-                ["Name"] = "STOCK",
-                ["Price"] = 123.45,
-                ["LastUpdated"] = currentTime
-            };
-
-            var mapper = new Mapper();
-
-            StockRespons expectedResponse = new StockRespons
-            {
-                Name = "STOCK",
-                Price = 123.45,
-                LastUpdated = currentTime
-            };
-
-            StockRespons result;
-
-            // Act
-            result = mapper.ToStockContract(item);
-
-            // Assert
-            Assert.AreEqual(expectedResponse.Name, result.Name);
-            Assert.AreEqual(expectedResponse.Price, result.Price);
-            Assert.AreEqual(expectedResponse.LastUpdated, result.LastUpdated);
-        }
-
-        [Test]
-        public void Mapper_ToDocument_ReturnsDocument()
-        {
-            // Arrange
-            string currentTime = System.DateTime.Now.ToString();
-
-            StockRespons stockResponse = new StockRespons
-            {
-                Name = "STOCK",
-                Price = 123.45,
-                LastUpdated = currentTime
-            };
-
-            var expectedDocument = new Document
-            {
-                ["Name"] = "STOCK",
-                ["Price"] = 123.45,
-                ["LastUpdated"] = currentTime
-            };
-
-            var mapper = new Mapper();
-
-            Document result;
-
-            // Act
-            result = mapper.ToDocument(stockResponse);
-            
-            // Assert
-            Assert.AreEqual(expectedDocument["Name"], result["Name"]);
-            Assert.AreEqual(expectedDocument["Price"], result["Price"]);
-            Assert.AreEqual(expectedDocument["LastUpdated"], result["LastUpdated"]);
-        }
-
-        [Test]
-        public void Mapper_ToStockContract_ReturnsStocks()
-        {
-            // Arrange
-            string currentTime = System.DateTime.Now.ToString();
-
-            var item1 = new Document();
-            item1["Name"] = "STOCK1";
-            item1["Price"] = 123.45;
-            item1["LastUpdated"] = currentTime;
-
-            var item2 = new Document();
-            item2["Name"] = "STOCK2";
-            item2["Price"] = 111.11;
-            item2["LastUpdated"] = currentTime;
-
-            List<Document> itemList = new List<Document>();
-            itemList.Add(item1);
-            itemList.Add(item2);
-
-            StockRespons expectedItem1 = new StockRespons();
-            expectedItem1.Name = "STOCK1";
-            expectedItem1.Price = 123.45;
-            expectedItem1.LastUpdated = currentTime;
-
-            StockRespons expectedItem2 = new StockRespons();
-            expectedItem2.Name = "STOCK2";
-            expectedItem2.Price = 111.11;
-            expectedItem2.LastUpdated = currentTime;
-
-            List<StockRespons> expectedItemList = new List<StockRespons>
-            {
-                expectedItem1,
-                expectedItem2
-            };
-
-            var mapper = new Mapper();
-
-            IEnumerable<StockRespons> result;
-
-            // Act
-            result = mapper.ToStockContract(itemList);
-
-            // Assert
-            var enumerator = result.GetEnumerator();
-            int count = -1;
-            while(enumerator.MoveNext())
-            {
-                count++;
-                Assert.AreEqual(expectedItemList[count].Name, enumerator.Current.Name);
-                Assert.AreEqual(expectedItemList[count].Price, enumerator.Current.Price);
-                Assert.AreEqual(expectedItemList[count].LastUpdated, enumerator.Current.LastUpdated);
-            }
-        }
+        public void Setup() { }
 
         [Test]
         public async Task StockRep_GetAllItems_ReturnsAllItems()
--- StockTrading.Sender.Tests/RepositoriesTests.cs
+++ StockTrading.Sender.Tests/RepositoriesTests.cs
@@ -1,84 +1,144 @@
+using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using NUnit.Framework;
 using StockTrading.Sender.Contracts;
 using StockTrading.Sender.Libs;
+using StockTrading.Sender.Libs.Repositories;
 using StockTrading.Sender.Libs.Models;
 using StockTrading.Sender.Mappers;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace StockTrading.Sender.Tests
 {
     public class RepositoriesTests
     {
         [SetUp]
-        public void Setup()
+        public void Setup(){}
+
+        [Test]
+        public async Task GetAllItems_Test()
         {
-            //instantiate StockTradingRepository
+            // Arrange
+            var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
+            
+            // Act
+            var result = await stockRep.GetAllItems();
+            
+            // Assert
+            var enumerator = result.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Assert.IsNotNull(enumerator.Current);
+            }
         }
 
         [Test]
-        public void GetAllItems_Test()
+        public async Task GetItem_Test()
         {
             // Arrange
+            string currentTime = System.DateTime.Now.ToString();
+            var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
+            var mapper = new Mapper();
 
+            var newStock = new StockDB();
+            newStock.Name = "SendStockTest";
+            newStock.Price = 123.45f;
+            newStock.LastUpdated = currentTime;
 
+            var mappedStock = mapper.ToStockContract(newStock);
+            
             // Act
-
-
+            await stockRep.AddStock(newStock);
+            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTest"));
+           
             // Assert
+            Assert.AreEqual(mappedStock.Name, result.Name);
+            Assert.AreEqual(mappedStock.Price, result.Price);
+            Assert.AreEqual(mappedStock.LastUpdated, result.LastUpdated);
+            
+            // Clean-up
+            await stockRep.DeleteStock(newStock);
+        }
+
+        [Test]
+        public async Task AddStock_Test()
+        {
+            // Arrange
+            string currentTime = System.DateTime.Now.ToString();
+            var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
+            var mapper = new Mapper();
+
+            var newStock = new StockDB();
+            newStock.Name = "SendStockTest";
+            newStock.Price = 123.45f;
+            newStock.LastUpdated = currentTime;
+
+            var mappedStock = mapper.ToStockContract(newStock);
+            
+            // Act
+            await stockRep.AddStock(newStock);
+            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTest"));
+            
+            // Assert
+            Assert.AreEqual(mappedStock.Name, result.Name);
+            Assert.AreEqual(mappedStock.Price, result.Price);
+            Assert.AreEqual(mappedStock.LastUpdated, result.LastUpdated);
+            
+            // Clean-up
+            await stockRep.DeleteStock(newStock);
 
         }
 
         [Test]
-        public void GetItem_Test()
+        public async Task DeleteStock_Test()
         {
             // Arrange
+            string currentTime = System.DateTime.Now.ToString();
+            var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
 
+            var newStock = new StockDB();
+            newStock.Name = "SendStockTest";
+            newStock.Price = 123.45f;
+            newStock.LastUpdated = currentTime;
 
             // Act
-
-
+            await stockRep.AddStock(newStock);
+            var result1 = await stockRep.GetItem(newStock.Name);
+            await stockRep.DeleteStock(newStock);
+            var result2 = await stockRep.GetItem(newStock.Name);
+            
             // Assert
+            Assert.AreNotEqual(result1, result2);
 
         }
 
         [Test]
-        public void AddStock_Test()
+        public async Task UpdateStock_Test()
         {
             // Arrange
+            var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
 
+            var oldStock = new StockDB();
+            oldStock.Name = "SendStockTest";
+            oldStock.Price = 123.45f;
+            oldStock.LastUpdated = System.DateTime.Now.ToString();
 
-            // Act
-
-
-            // Assert
-
-        }
-
-        [Test]
-        public void DeleteStock_Test()
-        {
-            // Arrange
-
+            var newStock = new StockDB();
+            newStock.Name = "SendStockTest";
+            newStock.Price = 678.90f;
+            newStock.LastUpdated = System.DateTime.Now.ToString();
 
             // Act
-
-
+            await stockRep.AddStock(oldStock);
+            var result1 = await stockRep.GetItem(oldStock.Name);
+            await stockRep.UpdateStock(newStock);
+            var result2 = await stockRep.GetItem(newStock.Name);
             // Assert
-
-        }
-
-        [Test]
-        public void UpdateStock_Test()
-        {
-            // Arrange
-
-
-            // Act
-
-
-            // Assert
-
+            Assert.AreNotEqual(result1.Price, result2.Price);
+            
+            // Clean-up
+            await stockRep.DeleteStock(newStock);
         }
     }
 }
