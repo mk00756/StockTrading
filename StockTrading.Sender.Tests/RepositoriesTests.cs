75a47870f60150047bf148b0447dd32d335071c5
nathanyip98 <nathanyip98@yahoo.co.uk>
2020-04-16 15:02:56 +0100

    Purge messages after tests

--- StockTrading.Sender.Tests/RepositoriesTests.cs
+++ StockTrading.Sender.Tests/RepositoriesTests.cs
@@ -8,6 +8,8 @@ using StockTrading.Sender.Libs.Models;
 using StockTrading.Sender.Mappers;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using StockTrading.Sender.Libs.MessageBroker;
+using RabbitMQ.Client;
 
 namespace StockTrading.Sender.Tests
 {
@@ -42,7 +44,7 @@ namespace StockTrading.Sender.Tests
             var mapper = new Mapper();
 
             var newStock = new StockDB();
-            newStock.Name = "SendStockTest";
+            newStock.Name = "SendStockTestDelete";
             newStock.Price = 123.45f;
             newStock.LastUpdated = currentTime;
 
@@ -50,7 +52,7 @@ namespace StockTrading.Sender.Tests
             
             // Act
             await stockRep.AddStock(newStock);
-            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTest"));
+            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTestDelete"));
            
             // Assert
             Assert.AreEqual(mappedStock.Name, result.Name);
@@ -59,18 +61,40 @@ namespace StockTrading.Sender.Tests
             
             // Clean-up
             await stockRep.DeleteStock(newStock);
+
+            // Clean up messages
+            // Initiate RabbitMQ 
+            RabbitMQClient client = new RabbitMQClient("Delete_Queue", "stock.delete");
+
+            ConnectionFactory _factory;
+            IConnection _connection;
+            IModel _model;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueuePurge("Add_Queue");
+            _model.QueuePurge("Delete_Queue");
+            _model.QueuePurge("Patch_Queue");
         }
 
         [Test]
         public async Task AddStock_Test()
         {
             // Arrange
+
             string currentTime = System.DateTime.Now.ToString();
             var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
             var mapper = new Mapper();
 
             var newStock = new StockDB();
-            newStock.Name = "SendStockTest";
+            newStock.Name = "SendStockTestAdd";
             newStock.Price = 123.45f;
             newStock.LastUpdated = currentTime;
 
@@ -78,7 +102,7 @@ namespace StockTrading.Sender.Tests
             
             // Act
             await stockRep.AddStock(newStock);
-            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTest"));
+            var result = mapper.ToStockContract(await stockRep.GetItem("SendStockTestAdd"));
             
             // Assert
             Assert.AreEqual(mappedStock.Name, result.Name);
@@ -88,17 +112,38 @@ namespace StockTrading.Sender.Tests
             // Clean-up
             await stockRep.DeleteStock(newStock);
 
+            // Clean up messages
+            // Initiate RabbitMQ 
+            RabbitMQClient client = new RabbitMQClient("Add_Queue", "stock.add");
+
+            ConnectionFactory _factory;
+            IConnection _connection;
+            IModel _model;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueuePurge("Add_Queue");
+            _model.QueuePurge("Delete_Queue");
+            _model.QueuePurge("Patch_Queue");
         }
 
         [Test]
         public async Task DeleteStock_Test()
         {
             // Arrange
+
             string currentTime = System.DateTime.Now.ToString();
             var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
 
             var newStock = new StockDB();
-            newStock.Name = "SendStockTest";
+            newStock.Name = "SendStockTestDelete";
             newStock.Price = 123.45f;
             newStock.LastUpdated = currentTime;
 
@@ -111,21 +156,42 @@ namespace StockTrading.Sender.Tests
             // Assert
             Assert.AreNotEqual(result1, result2);
 
+            // Clean up messages
+            // Initiate RabbitMQ 
+            RabbitMQClient client = new RabbitMQClient("Delete_Queue", "stock.delete");
+
+            ConnectionFactory _factory;
+            IConnection _connection;
+            IModel _model;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueuePurge("Add_Queue");
+            _model.QueuePurge("Delete_Queue");
+            _model.QueuePurge("Patch_Queue");
         }
 
         [Test]
         public async Task UpdateStock_Test()
         {
             // Arrange
+
             var stockRep = new StockTradingRepository(new AmazonDynamoDBClient());
 
             var oldStock = new StockDB();
-            oldStock.Name = "SendStockTest";
+            oldStock.Name = "SendStockTestUpdate";
             oldStock.Price = 123.45f;
             oldStock.LastUpdated = System.DateTime.Now.ToString();
 
             var newStock = new StockDB();
-            newStock.Name = "SendStockTest";
+            newStock.Name = "SendStockTestUpdate";
             newStock.Price = 678.90f;
             newStock.LastUpdated = System.DateTime.Now.ToString();
 
@@ -139,6 +205,28 @@ namespace StockTrading.Sender.Tests
             
             // Clean-up
             await stockRep.DeleteStock(newStock);
+
+            // Clean up messages
+            // Initiate RabbitMQ 
+            RabbitMQClient client = new RabbitMQClient("Patch_Queue", "stock.patch");
+
+            ConnectionFactory _factory;
+            IConnection _connection;
+            IModel _model;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueuePurge("Add_Queue");
+            _model.QueuePurge("Delete_Queue");
+            _model.QueuePurge("Patch_Queue");
+
         }
     }
 }
